8c270d96213038a5828a2412a48ed6be306a67c6
Viral Patel <pviral3011@gmail.com>
2024-11-25 18:28:24 +0530

    visual feedback to authentication

--- app/routes/_marketing.auth.jsx
+++ app/routes/_marketing.auth.jsx
@@ -5,6 +5,20 @@ export default function AuthPage() {
   return <AuthForm />;
 }
 
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const authMode = searchParams.get('mode') || 'login';
+
+  const formData = request.formData();
+  const credentials = Object.fromEntries(formData);
+
+  if (authMode === 'login') {
+    //login code
+  } else {
+    // signup code
+  }
+}
+
 export function links() {
   return [{ rel: 'stylesheet', href: authStyles }];
 }
